5644d117e9027f5a6bb9bc4b341d332ba7cb8128
Brian Goff <cpuguy83@gmail.com>
2019-03-25 11:23:35 -0700

    [chore] Use moby for GPU instances

--- packer/install-dependencies.sh
+++ packer/install-dependencies.sh
@@ -47,17 +47,16 @@ cat << EOF >> ${RELEASE_NOTES_FILEPATH}
   - zip
 EOF
 
+
+MOBY_VERSION="3.0.4"
+installMoby
+echo "  - moby v${MOBY_VERSION}" >> ${RELEASE_NOTES_FILEPATH}
+
 if [[ ${FEATURE_FLAGS} == *"docker-engine"* ]]; then
-    DOCKER_ENGINE_REPO="https://apt.dockerproject.org/repo"
-    installDockerEngine
     overrideDockerEngineStorageDriver
     echo "  - docker-engine v${DOCKER_ENGINE_VERSION}" >> ${RELEASE_NOTES_FILEPATH}
     installGPUDrivers
     echo "  - nvidia-docker2 nvidia-container-runtime" >> ${RELEASE_NOTES_FILEPATH}
-else
-    MOBY_VERSION="3.0.4"
-    installMoby
-    echo "  - moby v${MOBY_VERSION}" >> ${RELEASE_NOTES_FILEPATH}
 fi
 
 installClearContainersRuntime
